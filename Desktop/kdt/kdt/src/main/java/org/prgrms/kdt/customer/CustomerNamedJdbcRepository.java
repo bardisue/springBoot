f340da51380a2a0b33a225e26d8b1f84c6cd5b3f
delta <h0568@naver.com>
2022-12-29 17:24:09 +0900

    NamedParameterJdbcTemplate 1

---
+++ Desktop/kdt/kdt/src/main/java/org/prgrms/kdt/customer/CustomerNamedJdbcRepository.java
@@ -0,0 +1,140 @@
+package org.prgrms.kdt.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.util.*;
+
+@Repository
+public class CustomerNamedJdbcRepository implements CustomerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerNamedJdbcRepository.class);
+
+   // private final DataSource dataSource;
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private RowMapper<Customer> customerRowMapper = (resultSet, i) -> {
+        var customerName = resultSet.getString("name");
+        var email = resultSet.getString("email");
+        var customerId = toUUID(resultSet.getBytes("customer_id"));
+        var lastLoginAt = resultSet.getTimestamp("last_login_at") != null ?
+                resultSet.getTimestamp("last_login_at").toLocalDateTime() : null;
+        var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new Customer(customerId, customerName, email, lastLoginAt, createdAt);
+    };
+
+    public CustomerNamedJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private Map<String, Object> toParamMap(Customer customer){
+        return new HashMap<>(){{
+            put("customerId", customer.getCustomerId().toString().getBytes());
+            put("name", customer.getName());
+            put("email", customer.getEmail());
+            put("createdAt", Timestamp.valueOf(customer.getCreatedAt()));
+            put("lastLoginAt", customer.getLastLoginAt() != null ? Timestamp.valueOf(customer.getCreatedAt()) :null);
+        }};
+    }
+
+    @Override
+    public Customer insert(Customer customer) {
+        var update = jdbcTemplate.update("INSERT INTO customers(customer_id, name, email, created_at) VALUES (UNHEX(REPLACE(:customerId, '-', '')), :name, :email, :createdAt)",
+                toParamMap(customer));
+        /***
+        var update = jdbcTemplate.update("INSERT INTO customers(customer_id, name, email, created_at) VALUES (UNHEX(REPLACE(?, '-', '')), ?, ?, ?)",
+                customer.getCustomerId().toString().getBytes(),
+                customer.getName(),
+                customer.getEmail(),
+                Timestamp.valueOf(customer.getCreatedAt()));
+         ***/
+        if (update != 1){
+            throw new RuntimeException("Nothing was inserted");
+        }
+        return customer;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        var update = jdbcTemplate.update("UPDATE customers SET name = :name, email = :email, last_login_at = :lastLoginAt  WHERE customer_id = UNHEX(REPLACE(:customerId, '-', ''))",
+                toParamMap(customer));
+        /***
+        var update = jdbcTemplate.update("UPDATE customers SET name = ?, email = ?, last_login_at = ?  WHERE customer_id = UNHEX(REPLACE(?, '-', ''))",
+                customer.getName(),
+                customer.getEmail(),
+                customer.getLastLoginAt() != null ? Timestamp.valueOf(customer.getCreatedAt()) : null,
+                customer.getCustomerId().toString().getBytes()
+        );
+         ***/
+        if (update != 1){
+            throw new RuntimeException("Nothing was updated");
+        }
+        return customer;
+    }
+
+    @Override
+    public int count() {
+        return jdbcTemplate.queryForObject("select count(*) from customers", Collections.emptyMap() ,Integer.class);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return jdbcTemplate.query("select * from customers", customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers WHERE customer_id = UNHEX(REPLACE(:customerId, '-', ''))",
+                    Collections.singletonMap("customerId", customerId.toString().getBytes()),
+                    customerRowMapper));
+        } catch (EmptyResultDataAccessException e){
+            logger.error("Got Empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByName(String name) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers WHERE name = :name",
+                    Collections.singletonMap("name", name),
+                    customerRowMapper));
+        } catch (EmptyResultDataAccessException e){
+            logger.error("Got Empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByByEmail(String email) {
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers WHERE email = :email",
+                    Collections.singletonMap("email", email),
+                    customerRowMapper));
+        } catch (EmptyResultDataAccessException e){
+            logger.error("Got Empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        var update = jdbcTemplate.update("DELETE FROM customers", Collections.emptyMap());
+    }
+
+    static UUID toUUID(byte[] bytes){
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+}
