3d91397d7ff31ce2dd154354bdd495e9bfc739bf
delta <h0568@naver.com>
2023-01-17 11:35:03 +0900

    bardisueFirstcommit

---
+++ Desktop/everyTown/src/main/java/com/cnusw/everytown/EverytownApplication.java
@@ -0,0 +1,13 @@
+package com.cnusw.everytown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverytownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverytownApplication.class, args);
+	}
+
+}
---
+++ Desktop/everyTown/src/test/java/com/cnusw/everytown/EverytownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnusw.everytown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverytownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
